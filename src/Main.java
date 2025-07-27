9c8f2465eb801e09f198017386babbb9aae32b88
berk <118214474+littleborek@users.noreply.github.com>
2025-07-27 16:45:00 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         server.createContext("/", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
-                String response = "Hello from Java HTTP Server!";
+                String response = "Hello from Java HTTP Server.!";
                 exchange.sendResponseHeaders(200, response.getBytes().length);
                 OutputStream os = exchange.getResponseBody();
                 os.write(response.getBytes());
